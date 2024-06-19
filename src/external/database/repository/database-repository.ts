42d4a5cd8e3a4aa4ac0c8a33fae6c40c22ed0020
Dikendev <diken.dev@gmail.com>
2024-06-18 22:56:54 -0300

    database repository

--- src/external/database/repository/database-repository.ts
+++ src/external/database/repository/database-repository.ts
@@ -1,7 +1,9 @@
+import { InMemoryData, InMemoryDataResponseType } from '../database.service';
+
 export abstract class DataBaseRepository {
   abstract create(data: any): void;
-  abstract get(): void;
-  abstract update(): void;
+  abstract get(key: string): InMemoryDataResponseType;
+  abstract update(key: string, data: InMemoryData): void;
   abstract delete(): void;
   abstract list(): void;
 }

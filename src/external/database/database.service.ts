35ddd0dc2976934655f0019e9b2b40500382b63e
Dikendev <diken.dev@gmail.com>
2024-06-18 22:56:20 -0300

    feat add in memory data base

--- src/external/database/database.service.ts
+++ src/external/database/database.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { DataBaseRepository } from './repository/database-repository';
+
+export interface InMemoryData {
+  [key: string]: any;
+}
+
+export type InMemoryDataResponseType = InMemoryData | string;
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService implements DataBaseRepository {
+  inMemoryDatabase: InMemoryData[] = [];
+  constructor() {}
+
+  create(data: InMemoryData): void {
+    this.inMemoryDatabase.push(data);
+  }
+
+  get(key: string): InMemoryDataResponseType {
+    const assignNewData = Object.assign({}, ...this.inMemoryDatabase);
+    const keys = Object.keys(assignNewData);
+    const result = keys.find((data) => data === key);
+
+    if (!result) {
+      return 'Key not found';
+    }
+
+    return this.inMemoryDatabase[0];
+  }
+
+  update(key: string, data: InMemoryData): void {
+    throw new Error('Method not implemented.');
+  }
+  delete(): void {
+    throw new Error('Method not implemented.');
+  }
+  list(): void {
+    throw new Error('Method not implemented.');
+  }
+}

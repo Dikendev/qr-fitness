1462235cfe996367bf17db2b254a66f1475cbe2a
Dikendev <diken.dev@gmail.com>
2024-06-25 23:46:35 -0300

    add zod / global prefix / filter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { ZodFilter } from './filter/zod/zod.filter';
+
+const PORT = process.env.PORT || 3000;
 
 (async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = Logger;
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new ZodFilter());
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  await app.listen(PORT, () => {
+    logger.log(`Listening on: ${PORT}`, 'Bootstrap');
+  });
 })();

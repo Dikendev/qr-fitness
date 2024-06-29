71999712d9eb1198b3dffb4ff85b8bb36b34da43
Dikendev <diken.dev@gmail.com>
2024-06-28 23:57:15 -0300

    update create flow and list results

--- src/user/user-prisma.service.ts
+++ src/user/user-prisma.service.ts
@@ -2,19 +2,62 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository } from './repository/user.repository';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto, UserResponse } from './model/user.model';
+import { WorkoutPlan } from '../workout-plan/model/workout-plan-dto';
+import { Exercise } from '../exercise/model/exercise.model';
 
 @Injectable()
 export class UserPrismaService implements UserRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(body: CreateUserDto): Promise<UserResponse> {
+    console.log(body);
     try {
       const user = await this.prisma.user.create({
-        data: { email: body.email, name: body.name, password: body.password },
+        data: {
+          email: body.email,
+          name: body.name,
+          password: body.password,
+          workouts: {
+            create: body.workouts?.map((workout) => ({
+              name: workout.name,
+              description: workout.description,
+              workoutType: workout.workoutType,
+              exercises: {
+                create: workout.exercises?.map((exercise) => ({
+                  name: exercise.name,
+                  description: exercise.description,
+                  sets: exercise.sets,
+                  reps: exercise.reps,
+                })),
+              },
+            })),
+          },
+        },
         select: {
           id: true,
-          email: true,
           name: true,
+          email: true,
+          workouts: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              workoutType: true,
+              exercises: {
+                select: {
+                  id: true,
+                  name: true,
+                  description: true,
+                  sets: true,
+                  reps: true,
+                  createdAt: true,
+                  updatedAt: true,
+                },
+              },
+              createdAt: true,
+              updatedAt: true,
+            },
+          },
           createdAt: true,
           updatedAt: true,
         },
@@ -33,15 +76,35 @@ export class UserPrismaService implements UserRepository {
       const users = await this.prisma.user.findMany({
         select: {
           id: true,
-          email: true,
           name: true,
+          email: true,
+          workouts: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              workoutType: true,
+              createdAt: true,
+              updatedAt: true,
+              exercises: {
+                select: {
+                  id: true,
+                  name: true,
+                  description: true,
+                  sets: true,
+                  reps: true,
+                  createdAt: true,
+                  updatedAt: true,
+                },
+              },
+            },
+          },
           updatedAt: true,
           createdAt: true,
         },
       });
 
       if (!users) throw new BadRequestException('Users not found');
-
       return users;
     } catch (error) {
       throw error;
@@ -54,8 +117,29 @@ export class UserPrismaService implements UserRepository {
         where: { id },
         select: {
           id: true,
-          email: true,
           name: true,
+          email: true,
+          workouts: {
+            select: {
+              id: true,
+              name: true,
+              description: true,
+              workoutType: true,
+              createdAt: true,
+              updatedAt: true,
+              exercises: {
+                select: {
+                  id: true,
+                  name: true,
+                  description: true,
+                  sets: true,
+                  reps: true,
+                  createdAt: true,
+                  updatedAt: true,
+                },
+              },
+            },
+          },
           updatedAt: true,
           createdAt: true,
         },

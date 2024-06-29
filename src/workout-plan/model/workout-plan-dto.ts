c2ba8c3b62abf6285ef05db5eee824a70a59b619
Dikendev <diken.dev@gmail.com>
2024-06-28 23:57:35 -0300

    update workout pan dto

--- src/workout-plan/model/workout-plan-dto.ts
+++ src/workout-plan/model/workout-plan-dto.ts
@@ -1,16 +1,26 @@
 import { z } from 'zod';
-import { UserSchema } from '../../user/model/user.model';
-import { ExerciseSchema } from '../../exercice/model/model.model';
+import { Exercise, ExerciseSchema } from '../../exercise/model/exercise.model';
 
-export const WorkoutPlanSchema = z.object({
+export interface WorkoutPlan {
+  id: string;
+  name: string;
+  description: string;
+  workoutType: string;
+  userId?: string;
+  exercises?: Exercise[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const WorkoutPlanSchema: z.ZodSchema<WorkoutPlan> = z.object({
   id: z.string().cuid(),
   name: z.string(),
   description: z.string(),
   workoutType: z.string(),
   exercises: z.array(ExerciseSchema).optional(),
+  userId: z.string().optional(),
   createdAt: z.date(),
   updatedAt: z.date(),
-  userId: z.string().optional(),
 });
 
-export type WorkoutPlantDto = z.infer<typeof WorkoutPlanSchema>;
+export type WorkoutPlanDto = z.infer<typeof WorkoutPlanSchema>;

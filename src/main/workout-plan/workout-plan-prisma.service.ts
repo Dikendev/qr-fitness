d0f916c99071675b2fcb8e5560519eefeb52cd07
Dikendev <diken.dev@gmail.com>
2024-07-01 01:36:19 -0300

    update work plan service select / create / list methods

--- src/main/workout-plan/workout-plan-prisma.service.ts
+++ src/main/workout-plan/workout-plan-prisma.service.ts
@@ -1,14 +1,94 @@
 import { Injectable } from '@nestjs/common';
 import { WorkoutPlanRepository } from './repository/workout-plan-repository';
 import { PrismaService } from '../../prisma/prisma.service';
+import {
+  CreateWorkoutPlanDto,
+  WorkoutPlanResponse,
+} from './model/workout-plan-dto';
 
 @Injectable()
 export class WorkoutPlanService implements WorkoutPlanRepository {
+  exerciseSelect = {
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      sets: true,
+      reps: true,
+      updatedAt: true,
+      createdAt: true,
+    },
+  };
+
+  workoutPlanSelect = {
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      workoutType: true,
+      exercises: this.exerciseSelect,
+      createdAt: true,
+      updatedAt: true,
+    },
+  };
+
   constructor(private readonly prisma: PrismaService) {}
 
-  async createWorkoutPlan(): Promise<void> {
-    // const workoutPlan = await this.prisma.workout.create({ data: {} });
+  async create(body: CreateWorkoutPlanDto): Promise<WorkoutPlanResponse> {
+    try {
+      const workoutPlan = await this.prisma.$transaction(async (prisma) => {
+        const connectExercises = body.exercises
+          ?.filter((exercise) => exercise.id)
+          .map((exercise) => ({ id: exercise.id }));
+
+        const exercisesBulk = body.exercises?.map((exercise) => {
+          return {
+            where: { id: exercise.id ?? '' },
+            create: {
+              name: exercise.name ?? '',
+              description: exercise.description ?? '',
+              sets: exercise.sets ?? 0,
+              reps: exercise.reps ?? 0,
+            },
+          };
+        });
+
+        const workoutPlan = await prisma.workout.create({
+          data: {
+            name: body.name,
+            description: body.description,
+            workoutType: body.workoutType,
+            userId: body?.userId,
+            exercises: {
+              connect: connectExercises,
+              connectOrCreate: exercisesBulk,
+            },
+          },
+          select: this.workoutPlanSelect.select,
+        });
+
+        return workoutPlan;
+      });
+
+      return workoutPlan;
+    } catch (error) {
+      throw new Error('Failed to create workout plan');
+    }
   }
 
-  listWorkoutPlan(): void {}
+  async list(): Promise<WorkoutPlanResponse[]> {
+    try {
+      const exercises = this.prisma.workout.findMany({
+        select: this.workoutPlanSelect.select,
+      });
+
+      if (!exercises) {
+        throw new Error('No exercises found');
+      }
+
+      return exercises;
+    } catch (error) {
+      throw new Error();
+    }
+  }
 }
--- src/main/workout-plan/workout-plan.controller.ts
+++ src/main/workout-plan/workout-plan.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { WorkoutPlanRepository } from './repository/workout-plan-repository';
+import {
+  CreateWorkoutPlanDto,
+  WorkoutPlanCreateSchema,
+  WorkoutPlanResponse,
+} from './model/workout-plan-dto';
+import { ZodPipe } from '../../pipe/zod/zod.pipe';
 
 @Controller('workout-plan')
 export class WorkoutPlanController {
   constructor(private readonly workoutPlanRepository: WorkoutPlanRepository) {}
+
+  @Post()
+  async create(
+    @Body(new ZodPipe(WorkoutPlanCreateSchema))
+    body: CreateWorkoutPlanDto,
+  ): Promise<WorkoutPlanResponse> {
+    return this.workoutPlanRepository.create(body);
+  }
+
+  @Get('list')
+  async list(): Promise<WorkoutPlanResponse[]> {
+    return this.workoutPlanRepository.list();
+  }
 }

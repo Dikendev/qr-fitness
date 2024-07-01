2547236a283e7e4a980482830562db34af3c5457
Dikendev <diken.dev@gmail.com>
2024-07-01 01:34:46 -0300

    add workout plan import prisma module

--- src/main/workout-plan/workout-plan.module.ts
+++ src/main/workout-plan/workout-plan.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkoutPlanService } from './workout-plan-prisma.service';
 import { WorkoutPlanController } from './workout-plan.controller';
 import { WorkoutPlanRepository } from './repository/workout-plan-repository';
-import { DatabaseModule } from '../../external/database/database.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  imports: [DatabaseModule, PrismaModule],
+  imports: [PrismaModule],
   providers: [
     WorkoutPlanService,
     { provide: WorkoutPlanRepository, useClass: WorkoutPlanService },

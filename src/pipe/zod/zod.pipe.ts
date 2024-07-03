0eb61a154a35f0a89188e3d4c9ae430cb063716d
Dikendev <diken.dev@gmail.com>
2024-07-02 21:29:23 -0300

    fix zod pipe

--- src/pipe/zod/zod.pipe.ts
+++ src/pipe/zod/zod.pipe.ts
@@ -1,11 +1,20 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
 import { z } from 'zod';
 
 @Injectable()
 export class ZodPipe implements PipeTransform {
   constructor(private readonly schema: z.Schema) {}
   transform(value: any, metadata: ArgumentMetadata) {
-    this.schema.safeParse(value);
+    const safeParse = this.schema.safeParse(value);
+
+    if (!safeParse.success) {
+      throw new BadRequestException(safeParse.error.errors[0]);
+    }
     return value;
   }
 }

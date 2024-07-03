4ea1fe978aecac1b016c43484684e3c5e87c825e
Dikendev <diken.dev@gmail.com>
2024-07-02 21:30:49 -0300

    add prisma error when validations are not meeting the required

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -1,4 +1,9 @@
-import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
@@ -6,23 +11,48 @@ import { prismaErrorGenerate } from './prisma-error-generate';
 
 const PRISMA_ERRORS = prismaErrorGenerate();
 
-@Catch(Prisma.PrismaClientKnownRequestError)
-export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+@Catch()
+export class PrismaClientExceptionFilter
+  extends BaseExceptionFilter
+  implements ExceptionFilter
+{
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const message = this.shortMessage(exception.message);
 
-    if (PRISMA_ERRORS[exception.code]) {
-      const status = HttpStatus.CONFLICT;
-      response.status(status).json({
-        statusCode: status,
-        message,
-      });
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      if (PRISMA_ERRORS[exception.code]) {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message,
+        });
 
-      return status;
+        return status;
+      }
+    }
+
+    if (
+      exception instanceof Error &&
+      exception instanceof Prisma.PrismaClientValidationError
+    ) {
+      response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: this.shortMessagePrismaErrors(exception.message),
+      });
+    }
+
+    super.catch(exception, host);
+  }
+
+  private shortMessagePrismaErrors(message: string): string {
+    const regex = /Argument\s(.*)/;
+    const match = message.match(regex);
+    if (match) {
+      return match[1];
     } else {
-      super.catch(exception, host);
+      return 'No match found';
     }
   }
 
